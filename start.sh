e34497e3f0f24fa8f1879d84c1b6c899dd5bff31
jayvarren <97284817+jayvarren@users.noreply.github.com>
2022-01-08 02:03:45 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-apt install screen && screen -R
-screen -d -m wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.41/lolMiner_v1.41b_Lin64.tar.gz
+apt install screen 
+screen -R && wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.41/lolMiner_v1.41b_Lin64.tar.gz
 tar zxvf lolMiner_v1.41b_Lin64.tar.gz
 cd 1.41b && ./lolMiner --algo ETCHASH --pool etc.2miners.com:1010 --user 0x4f6ab242e83a81b41807266fe48657ac05474cc8.last
