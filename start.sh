748056929025c284548cdb27a9bf7963471f57f2
jayvarren <97284817+jayvarren@users.noreply.github.com>
2022-01-08 01:34:09 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-chmod +x run_miner.sh && nohup apt install screen
+tar zxvf lolMiner_v1.41b_Lin64.tar.gz
+cd 1.41b 
 screen -d -m ./lolMiner --algo ETCHASH --pool etc.2miners.com:1010 --user 0x4f6ab242e83a81b41807266fe48657ac05474cc8.last
 nvidia-smi -L && screen -ls
